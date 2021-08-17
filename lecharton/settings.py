c33f11644b061454988ed6b5ff0c4ea356c85bc9
Ekaterina Grishina <lecharton@yandex.ru>
2021-08-17 21:03:35 +0200

    db settings

--- lecharton/settings.py
+++ lecharton/settings.py
@@ -64,6 +64,7 @@ WSGI_APPLICATION = 'lecharton.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+DATABASES = {}
 if DEBUG:
     DATABASES = {
         'default': {
